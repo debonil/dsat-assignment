26878906e1fbb78c0a19eafc4a64aa37cb4fa46d
Debonil Ghosh <debonil.ghosh@gmail.com>
2022-01-17 19:24:39 +0530

    q1

---
+++ dsat-assign-2/ques-a01.c
@@ -0,0 +1,31 @@
+/*
+Q. Implement the following search algorithms from scratch:
+     1) Linear Search Algorithm
+
+ Submitted by Debonil Ghosh (M21AIE225)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linearSearch(int n, int *arr, int k)
+{
+    for (int i = 0; i < n; i++)
+        if (arr[i] == k)
+            return i;
+    return -1;
+}
+
+int main()
+{
+    int n, *arr, k;
+    scanf("%d", &n);
+    arr = (int *)calloc(n, sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &k);
+    printf("%d", linearSearch(n, arr, k));
+    return 0;
+}
